06bb6170fa800da1e717fbae24584e1686688e1b
zhangJiaLe <javabeanzjl@163.com>
2025-03-06 09:55:18 +0800

    feat(.gitignore)：修改.gitignore文件

---
+++ day2/src/main/java/Main.java
@@ -0,0 +1,15 @@
+/**
+ * @author 程序员阿乐
+ * @description
+ * @createDate 2025/3/6 09:47
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
