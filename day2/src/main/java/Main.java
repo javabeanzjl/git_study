4c51c84a9cd2b46dc12fa9e455b415e8dedc9ae4
zhangJiaLe <javabeanzjl@163.com>
2025-03-10 08:31:54 +0800

    feat(Main.java)：添加功能B：新增变量

--- day2/src/main/java/Main.java
+++ day2/src/main/java/Main.java
@@ -12,4 +12,17 @@ public class Main {
     public static int add(int a, int b) {
         return a + b;
     }
+
+    // 功能A：新增方法
+    public void featureA() { System.out.println("Feature A"); }
+
+    // 调试代码（未完成）
+    public void debugMethod() { /* TODO */ }
+
+    // 功能B：新增变量
+    private int count = 0;
+    private int count1 = 0;
+    private int count2 = 0;
+    private int count3 = 0;
 }
+
