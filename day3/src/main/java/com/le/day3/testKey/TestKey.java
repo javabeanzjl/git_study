81f219a7172f93a75e5bdf384ea79737cb27072f
zhangJiaLe <javabeanzjl@163.com>
2025-03-14 19:05:16 +0800

    feat(TestKey.java): 测试 git-secrets 拦截

---
+++ day3/src/main/java/com/le/day3/testKey/TestKey.java
@@ -0,0 +1,13 @@
+package com.le.day3.testKey;
+
+/**
+ * 测试git secrets 的作用
+ * @author 程序员阿乐
+ * @description
+ * @createDate 2025/3/14 19:03
+ */
+public class TestKey {
+
+    String awsKey = "AAS1214ASDSASFFGAWGGFGHDG324";
+    String awsSecret = "admin2345";
+}
