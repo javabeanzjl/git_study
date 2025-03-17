221feaf280e5ca8697588ed9285420faa9d87b8f
zhangJiaLe <javabeanzjl@163.com>
2025-03-17 08:42:38 +0800

    feat(Calculator.java): add "hello world"

---
+++ day4/src/main/java/com/le/day4/Calculator.java
@@ -0,0 +1,13 @@
+package com.le.day4;
+
+/**
+ * @author 程序员阿乐
+ * @description
+ * @createDate 2025/3/17 08:40
+ */
+public class Calculator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
