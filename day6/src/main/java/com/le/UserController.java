94a02184c5e78db4719811c30102ae303c836c54
zhangJiaLe <javabeanzjl@163.com>
2025-04-09 18:47:43 +0800

    feat(UserController.java): 提供用户登录、注册相关接口

---
+++ day6/src/main/java/com/le/UserController.java
@@ -0,0 +1,13 @@
+package com.le;
+
+/**
+ * @author 程序员阿乐
+ * @version 1.0
+ * @date 2025-04-09
+ * @description 提供用户登录、注册相关接口
+ */
+public class UserController {
+
+    public void login() {}
+    public void register() {}
+}
